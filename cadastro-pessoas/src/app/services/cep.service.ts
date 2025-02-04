0837fdda3a81d856ce6a415517ae6024683ba40e
diegoNZ04 <diegoamorim03152004@gmail.com>
2025-02-04 07:24:04 -0300

    Arrumando dependencias

--- cadastro-pessoas/src/app/services/cep.service.ts
+++ cadastro-pessoas/src/app/services/cep.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@angular/core";
 import { CepType } from "../types/cep.type";
 import { HttpClient } from "@angular/common/http";
-import { response } from "express";
 import { map } from "rxjs";
 
 @Injectable({ providedIn: 'root' })
